61d47068a6f0020350ad1442775eafeda79fb96f
mpn <imoney91@me.com>
2021-03-13 16:50:47 +0100

    setup

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/singleHello")
+	public String hello(){
+		return "Hello world";
+	}
+
+	@GetMapping()
+	public List<Student> getStudents(){
+		return this.studentService.getStudents();
+	}
+    
+}
